ba4742295adcd67ebdd3759102c1b74ee743f8f9
TinCode1998 <nelsonmouatvergara@gmail.com>
2024-09-23 19:09:47 -0300

    tgyhu

--- Contenedor/Taller/settings.py
+++ Contenedor/Taller/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'Taller.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
