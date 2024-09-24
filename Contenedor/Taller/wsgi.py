69cb66127fc1bae5215791ec7cb42d7e9f9f5be0
TinCode1998 <nelsonmouatvergara@gmail.com>
2024-09-24 14:49:27 -0300

    vover

--- Contenedor/Taller/wsgi.py
+++ Contenedor/Taller/wsgi.py
@@ -8,12 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taller.settings')
 
 application = get_wsgi_application()
-
-application = DjangoWhiteNoise(application)
