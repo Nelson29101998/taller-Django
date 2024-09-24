d863cc7bac46ac47e2e2ccbc2d69b811e057a822
TinCode1998 <nelsonmouatvergara@gmail.com>
2024-09-24 16:23:01 -0300

    polkmnj

--- Contenedor/Taller/urls.py
+++ Contenedor/Taller/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 from App1.views import IndexView
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("App1.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
